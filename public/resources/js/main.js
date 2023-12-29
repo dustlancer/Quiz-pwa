6c2dc34cc530b75f592bf8f58467a5d8a25cf5a0
dustlancer <andreyalp@me.com>
2023-12-29 13:43:23 +0300

    initial-commit

---
+++ public/resources/js/main.js
@@ -0,0 +1,9 @@
+// 'use strict';
+
+const SCREEN_WIDTH = window.innerWidth;
+
+let todoField = document.querySelector('.todoField');
+let todoSubmit = document.querySelector('.todoSubmit');
+let todos = document.querySelector('.todos');
+
+
